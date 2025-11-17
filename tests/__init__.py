188ad3b84a720b2081b358ddce4cfa34ae8b893e
ukrfgroup-eng <uk.rfgroup@gmail.com>
2025-11-17 04:08:13 +0300

    Update __init__.py
    Исправлен tests/__init__.py - убрано некорректное содержимое

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,14 +1,1 @@
-python
-"""
-Модуль тестирования MATRIX CORE.
 
-Содержит unit-тесты и интеграционные тесты для проверки функциональности системы.
-"""
-
-import os
-import sys
-
-# Добавляем корневую директорию в путь для импортов
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-__all__ = []
