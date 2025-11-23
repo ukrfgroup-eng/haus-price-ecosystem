b6198c407066dd5ca7288343f109e17a54a905c5
ukrfgroup-eng <uk.rfgroup@gmail.com>
2025-11-23 15:09:55 +0300

    Update __init__.py

--- backend/utils/__init__.py
+++ backend/utils/__init__.py
@@ -1,20 +1,13 @@
 """
-Модуль утилит MATRIX CORE.
-
-Содержит вспомогательные функции, хелперы и утилиты для работы системы.
+Utils package for MATRIX CORE
 """
 
-from .ai_helpers import analyze_user_intent, build_response, find_relevant_partners
-from .data_helpers import normalize_user_data, validate_request, format_response
-from .webhook_helpers import process_webhook, verify_signature
+from .ai_helpers import analyze_user_intent, extract_entities, find_relevant_partners
+from .data_helpers import validate_user_data, normalize_region
+from .webhook_helpers import process_umniko_webhook, process_tilda_webhook
 
 __all__ = [
-    'analyze_user_intent',
-    'build_response', 
-    'find_relevant_partners',
-    'normalize_user_data',
-    'validate_request',
-    'format_response',
-    'process_webhook',
-    'verify_signature'
+    'analyze_user_intent', 'extract_entities', 'find_relevant_partners',
+    'validate_user_data', 'normalize_region', 
+    'process_umniko_webhook', 'process_tilda_webhook'
 ]
