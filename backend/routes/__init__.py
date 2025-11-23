fc0e972f9e595755764fa7d24d8322f8419a1781
ukrfgroup-eng <uk.rfgroup@gmail.com>
2025-11-23 15:59:09 +0300

    Delete backend directory

--- backend/routes/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Routes package for MATRIX CORE
-"""
-
-from .webhook_routes import webhook_bp
-
-__all__ = ['webhook_bp']
--- backend/user_models.py
+++
@@ -1,7 +0,0 @@
-"""
-Файл для обратной совместимости - редирект на правильный путь к моделям
-"""
-
-from backend.models.user_models import User, UserRequest, UserProfile
-
-__all__ = ['User', 'UserRequest', 'UserProfile']
