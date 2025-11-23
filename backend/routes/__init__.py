b9bf9c006950333eb222d3925b0256e70376c678
ukrfgroup-eng <uk.rfgroup@gmail.com>
2025-11-23 14:58:09 +0300

    Update __init__.py

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -1,29 +1,7 @@
 """
-Модуль маршрутов API MATRIX CORE.
-
-Определяет все API endpoint-ы системы.
+Routes package for MATRIX CORE
 """
 
-from flask import Blueprint
+from .webhook_routes import webhook_bp
 
-# Создаем основной Blueprint для API
-api_bp = Blueprint('api', __name__)
-
-# Импортируем все маршруты
-from . import analysis_routes
-from . import partner_routes  
-from . import user_routes
-from . import connection_routes
-
-# Регистрируем модули маршрутов
-from .analysis_routes import analysis_bp
-from .partner_routes import partner_bp
-from .user_routes import user_bp
-from .connection_routes import connection_bp
-
-api_bp.register_blueprint(analysis_bp, url_prefix='/analyze')
-api_bp.register_blueprint(partner_bp, url_prefix='/partners')
-api_bp.register_blueprint(user_bp, url_prefix='/users')
-api_bp.register_blueprint(connection_bp, url_prefix='/connect')
-
-__all__ = ['api_bp']
+__all__ = ['webhook_bp']
